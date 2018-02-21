689407d3a61407505eb33338af33e6e9e6775f37
BloodyLink <david.quezada.m@gmail.com>
2018-02-21 10:11:12 -0300

    beautifying

--- script.php
+++ script.php
@@ -4,24 +4,25 @@ require_once('ConnectionDAO.php');
 
 $connectionDao = new ConnectionDAO();
 
-$xml_data = file_get_contents('php://input'); 
+$xml_data = file_get_contents('php://input');
 
-$xml = simplexml_load_string($xml_data);
-$json = json_encode($xml);//CONVERTIMOS A JSON
-$clienteData = json_decode($json,TRUE);//DE JSON PASAMOS A UN ARRAY
+$xml         = simplexml_load_string($xml_data);
+$json        = json_encode($xml); 
+$clienteData = json_decode($json, TRUE); 
 
 $cliente = $clienteData["acceso"]["cliente"];
 
- $pdo = $connectionDao->getPDO();
+$pdo = $connectionDao->getPDO();
 
- try{
-     $pdo->begintransaction();
-     $sql = "select next value for dbo.seq_webservice as codigo";
-     $q = $pdo->query($sql);
-     $res = $q->fetch();    
-		$pdo->commit();
- }catch(exception $e){
-     throw new exception("hubo un problema al obtener codigo." . $e->getmessage());
+try {
+    $pdo->begintransaction();
+    $sql = "select next value for dbo.seq_webservice as codigo";
+    $q   = $pdo->query($sql);
+    $res = $q->fetch();
+    $pdo->commit();
+}
+catch (exception $e) {
+    throw new exception("hubo un problema al obtener codigo." . $e->getmessage());
 }
 
 $codigoRegistro = $res["codigo"];
@@ -30,61 +31,63 @@ $apodo = "AXA_CO";
 
 $tableName = "tmp_ws_" . $apodo;
 
-$tags = null;
+$tags   = null;
 $values = null;
-foreach($clienteData as $secciones){
-    foreach($secciones as $keys => $data){
+foreach ($clienteData as $secciones) {
+    foreach ($secciones as $keys => $data) {
         
-		if($keys != "cliente"){//temporal... consultar por columna cliente
-		$tags .= ", " . $keys;
-			if(is_array($data)){
-            $values .=  ", NULL";
-            }else{
-                $values .=  ", '" . $data . "'";
+        if ($keys != "cliente") { //temporal... consultar por columna cliente
+            $tags .= ", " . $keys;
+            if (is_array($data)) {
+                $values .= ", NULL";
+            } else {
+                $values .= ", '" . $data . "'";
             }
-		}
+        }
         
     }
 }
 
 $columns = substr($tags, 2);
-$values = substr($values, 2);
+$values  = substr($values, 2);
 
-try{
-      $pdo->beginTransaction();
-      $sql = "INSERT INTO $tableName (COD_REGISTRO, $columns) VALUES ($codigoRegistro, $values);";
-
-      $q = $pdo->query($sql);  
-	  $pdo->commit();
- }catch(exception $e){
-   throw new Exception("Hubo un problema al insertar en tabla temporal." . $e->getMessage());
- }
+try {
+    $pdo->beginTransaction();
+    $sql = "INSERT INTO $tableName (COD_REGISTRO, $columns) VALUES ($codigoRegistro, $values);";
+    
+    $q = $pdo->query($sql);
+    $pdo->commit();
+}
+catch (exception $e) {
+    throw new Exception("Hubo un problema al insertar en tabla temporal." . $e->getMessage());
+}
 echo $cliente . " - " . $codigoRegistro;
 $respuesta = 0;
- try{
+try {
+    
+    $insert = "DECLARE    @return_value int,
+                        @respuesta varchar(max)
 
-	$insert = "DECLARE	@return_value int,
-						@respuesta varchar(max)
+                EXEC    @return_value = SP_carga_webservice
+                        @cliente = N'$cliente',
+                        @registro = $codigoRegistro,
+                        @respuesta = @respuesta OUTPUT
 
-				EXEC	@return_value = SP_carga_webservice
-						@cliente = N'$cliente',
-						@registro = $codigoRegistro,
-						@respuesta = @respuesta OUTPUT
-
-				SELECT	@respuesta as N'@respuesta'";
-$bdd = $connectionDao->getPDO();
-$stmt = $bdd->prepare($insert);     
-$stmt->execute();
-$tabResultat = $stmt->fetch();
-$resultSP = $tabResultat["@respuesta"];
-
- }catch(exception $e){
-     throw new Exception("Hubo un problema al llamar procedimiento." . $e->getMessage());
+                SELECT    @respuesta as N'@respuesta'";
+    $bdd    = $connectionDao->getPDO();
+    $stmt   = $bdd->prepare($insert);
+    $stmt->execute();
+    $tabResultat = $stmt->fetch();
+    $resultSP    = $tabResultat["@respuesta"];
+    
+}
+catch (exception $e) {
+    throw new Exception("Hubo un problema al llamar procedimiento." . $e->getMessage());
 }
 
 $arrayResultSP = explode("-", $resultSP);
-$estado = $arrayResultSP[0];
-$descripcion = $arrayResultSP[1];
+$estado        = $arrayResultSP[0];
+$descripcion   = $arrayResultSP[1];
 $responseArray = array(
     "respuesta" => array(
         "folio" => $clienteData["solicitud"]["folio"],
@@ -94,25 +97,24 @@ $responseArray = array(
     )
 );
 
-function array_to_xml( $data, &$xml_data ) {
-    foreach( $data as $key => $value ) {
-        if( is_numeric($key) ){
-            $key = 'item'.$key; 
+function array_to_xml($data, &$xml_data)
+{
+    foreach ($data as $key => $value) {
+        if (is_numeric($key)) {
+            $key = 'item' . $key;
         }
-        if( is_array($value) ) {
+        if (is_array($value)) {
             $subnode = $xml_data->addChild($key);
             array_to_xml($value, $subnode);
         } else {
-            $xml_data->addChild("$key",htmlspecialchars("$value"));
+            $xml_data->addChild("$key", htmlspecialchars("$value"));
         }
-     }
+    }
 }
 
 $xml_data = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><data></data>');
 
-array_to_xml($responseArray,$xml_data);
+array_to_xml($responseArray, $xml_data);
 
 header("content-type: text/xml; charset=utf-8");
 echo $xml_data->asxml();
-
-
